0430ce14033dfb28dde6ce06475829d2d3aa77aa
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-01 14:13:31 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,6 +5,30 @@ local Window = OrionLib:MakeWindow({IntroText = "Nexer Hub 👏", IntroIcon = "r
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
+local Script = Window:MakeTab({
+	Name = "Script",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Scripts = Script:AddSection({
+	Name = "some cool scripts"
+})
+
+Scripts:AddButton({
+	Name = "giang hub",
+	Callback = function()
+			 loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Slap_Battles/main/Slap_Battles.lua"))()
+	 end
+})
+
+Scripts:AddButton({
+	Name = "fly gui v3",
+	Callback = function()
+			 loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+	 end
+})
+
 local Tab = Window:MakeTab({
 	Name = "Badges",
 	Icon = "rbxassetid://4483345998",
